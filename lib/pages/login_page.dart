6ea3eee4ea6675df89855b99aede04a3bf1063cb
Aditya Anand <adianand2002@gmail.com>
2022-05-26 14:42:07 +0530

    day 3

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: const Text(
+          'Login Page',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.deepPurple,
+            fontWeight: FontWeight.bold,
+          ),
+          textScaleFactor: 2,
+        ),
+      ),
+    );
+  }
+}
