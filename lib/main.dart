f963adc6ff4572da5f28701bd8ad3dd8e0cd90ca
Aditya Anand <adianand2002@gmail.com>
2022-05-27 13:15:18 +0530

    theme extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/pages/home_page.dart';
 import 'package:myapp/pages/login_page.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 import 'package:myapp/utlx/routes.dart';
+import 'package:myapp/widgets/themes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,16 +14,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // vegetable(thaila: true);
     return MaterialApp(
       //home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-          primarySwatch: Colors.lightBlue,
-          fontFamily: GoogleFonts.lato().fontFamily),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
       initialRoute: MyRoute.homeRoute,
       routes: {
@@ -32,8 +28,4 @@ class MyApp extends StatelessWidget {
       },
     );
   }
-
-  // void vegetable({required bool thaila, int rupees = 100}) {
-  //   print("Going for market with Rs $rupees and thaila=$thaila");
-  // }
 }
---
+++ lib/widgets/themes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTheme {
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
+      primarySwatch: Colors.lightBlue,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      appBarTheme: AppBarTheme(
+        color: Colors.white,
+        elevation: 0.0,
+      ));
+
+  static ThemeData darkTheme(BuildContext context) => ThemeData(
+        brightness: Brightness.dark,
+      );
+}
