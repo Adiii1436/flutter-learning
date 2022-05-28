fd161d4760c2393dd0e0f097df708406cb4ee0f8
Aditya Anand <adianand2002@gmail.com>
2022-05-28 13:19:56 +0530

    day 10

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/models/catalog.dart';
 import 'package:myapp/widgets/drawer.dart';
+import 'package:myapp/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    int days = 30;
+    final dummyList = List.generate(8, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
       ),
-      body: Center(child: Text("Welcome to $days days of flutter")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
+      ),
       drawer: MyDrawer(),
     );
   }
