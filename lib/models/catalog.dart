77e3604f12c486ec643be4bc715cc4ec8418d86c
Aditya Anand <adianand2002@gmail.com>
2022-05-28 13:19:22 +0530

    day 10

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,10 +1,17 @@
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
-  final num price;
+  final String price;
   final String color;
   final String image;
 
   Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+}
+
+class CatalogModel {
+  static final items = [
+    Item(1, 'iPhone 13 Pro', 'Apple iphone 13th generation', '900', '#33505a',
+        "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-pro-family-hero?wid=470&hei=556&fmt=png-alpha&.v=1644969385433"),
+  ];
 }
