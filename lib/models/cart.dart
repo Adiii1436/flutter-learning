65e04700a852912ad4e5aa312e540218675362d5
Aditya Anand <adianand2002@gmail.com>
2022-05-30 12:22:18 +0530

    day 20 complete

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,6 +1,14 @@
+import 'dart:convert';
+
 import 'package:myapp/models/catalog.dart';
 
 class CartModel {
+  static final cartModel = CartModel._internal();
+
+  CartModel._internal();
+
+  factory CartModel() => cartModel;
+
   late CatalogModel _catalog;
 
   final List<int> _itemIds = [];
@@ -14,7 +22,7 @@ class CartModel {
   List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
 
   num get totalPrize =>
-      items.fold(0, (total, current) => total + current.price);
+      items.fold(0, (total, current) => total + int.parse(current.price));
 
   void add(Item item) {
     _itemIds.add(item.id);
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/models/cart.dart';
 import 'package:myapp/models/catalog.dart';
 import 'package:myapp/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -28,10 +29,11 @@ class _CartTotal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _cart = CartModel();
     return SizedBox(
       height: 200,
       child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-        "\$9000".text.xl5.color(context.accentColor).make(),
+        "\$${_cart.totalPrize}".text.xl5.color(context.accentColor).make(),
         30.widthBox,
         ElevatedButton(
                 onPressed: (() {
@@ -55,14 +57,16 @@ class _CartList extends StatefulWidget {
 }
 
 class __CartListState extends State<_CartList> {
+  final _cart = CartModel();
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: 5,
+        itemCount: _cart.items.length,
         itemBuilder: (context, index) => ListTile(
               leading: Icon(Icons.done),
               trailing: IconButton(onPressed: () {}, icon: Icon(Icons.remove)),
-              title: "Item 1".text.make(),
+              title: _cart.items[index].name.text.make(),
             ));
   }
 }
